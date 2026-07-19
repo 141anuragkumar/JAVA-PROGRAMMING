1111f01c114cd9ade839fd77ba57f4892be1a045
Anurag kumar <anuragk06469@gmail.com>
2026-07-19 17:22:43 +0530

    Array(sorted or not)

--- tcs_nqt/shorted_or_not.java
+++ tcs_nqt/shorted_or_not.java
@@ -4,21 +4,19 @@ public class shorted_or_not {
     public static void main(String[] args) {
         int[] number = {2,4,6,8,10};
 
-        boolean isShorted = true; //maan lo ki array pahale se hi sorted hai ye code ko choota karne me help karega
-        
-        for(int i=0;i<number.length -1;i++){
+        boolean isSorted = true;
+
+        for(int i=0;i<number.length-1;i++){
             if(number[i] > number[i+1]){
-                isShorted = false;
+                isSorted = false;
                 break;
             }
-            
         }
-        if(isShorted){
+        if(isSorted){
             System.out.println("Array is sorted");
         }
         else{
-            System.out.println("Array is unsorted");
+            System.out.println("Array is not sorted");
         }
-        
     }
 }
