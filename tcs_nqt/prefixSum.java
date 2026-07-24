877c6cf5dc5c2c1bc4e3b425ae4bbd5460146cc9
Anurag kumar <anuragk06469@gmail.com>
2026-07-25 05:07:12 +0530

    prefix sum

---
+++ tcs_nqt/prefixSum.java
@@ -0,0 +1,23 @@
+package tcs_nqt;
+
+public class prefixSum {
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5,6}; //Expected 1,3,6,10,15,21
+
+        int n = arr.length;
+        int[] prefix = new int[n];
+         
+        prefix[0] = arr[0];
+
+        for(int i = 1 ; i < n ; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+
+        }
+        System.out.println("Prefix array");
+
+        for(int num : prefix){
+            System.out.print(num + " ");
+        }
+    }
+}
